2a8b43552a8fc21bc0acda42df5e59065878cda4
Arthur <paracidex@gmail.com>
2024-01-18 09:04:37 -0600

    xsl project

---
+++ src/home/first.ts
@@ -0,0 +1,9 @@
+const num1: number = 20;
+const num2: number = 30;
+
+// Find the total
+const total: number = num1 + num2;
+
+// Find the average
+const average: number = total / 2;
+console.log("Total: ", total, "Average: ", average);
