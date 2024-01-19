4a05b3d5dc9f884032442f9d6a39b0969b2836d1
Arthur <paracidex@gmail.com>
2024-01-18 23:55:37 -0600

    xsl project 1

---
+++ non-react/src/basic/first.js
@@ -0,0 +1,9 @@
+const num1 = 20;
+const num2 = 30;
+
+// Find the total
+const total = num1 + num2;
+
+// Find the average
+const average = total / 2;
+console.log("Total: ", total, "Average: ", average);
---
+++ src/course/basic/first.ts
@@ -0,0 +1,6 @@
+const num1: number = 20;
+const num2: number = 30;
+
+const sum: number = num1 + num2;
+const avg: number = sum / 2;
+console.log("sum: " + sum + " avg: " + avg)
